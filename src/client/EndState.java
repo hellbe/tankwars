3bfb306ff92db251571374938db8134d34d08af5
Peter Leung <peterleung89@gmail.com>
2013-04-16 10:58:56 +0200

    Uppdaterat EndState
    Även tagit bort plane.png

--- src/client/EndState.java
+++ src/client/EndState.java
@@ -1,5 +1,37 @@
 package client;
 
-public class EndState {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class EndState extends BasicGameState {
+
+    int stateID = -1;
+	
+	public EndState( int stateID ){
+		this.stateID = stateID;
+	}
+	
+	@Override
+	public int getID() {
+		return stateID;
+	}
+	
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+		
+	}
 
 }
