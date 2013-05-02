583a04cf6dfde81c83a6aaf99324a1fa6c486fd7
Peter Leung <peterleung89@gmail.com>
2013-05-02 23:00:15 +0200

    Luddes PowerUpEntity

---
+++ src/entity/PowerUpEntity.java
@@ -0,0 +1,12 @@
+package entity;
+
+import org.newdawn.slick.geom.Vector2f;
+
+public class PowerUpEntity extends BlockEntity {
+
+	public PowerUpEntity(String id, Vector2f pos, Vector2f size) {
+		super(id, pos, size);
+	}
+	
+	
+}
