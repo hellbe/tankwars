d06487731cfdf66863123aa785c72dd2334c18e1
Peter Leung <peterleung89@gmail.com>
2013-05-07 19:13:01 +0200

    test

--- src/client/TWClient.java
+++ src/client/TWClient.java
@@ -24,10 +24,10 @@ public class TWClient extends StateBasedGame {
     }
  
     public void initStatesList(GameContainer gameContainer) throws SlickException {
-    	this.addState(new MapMenuState(1));
-//    	this.addState(new MenuState(MAINMENUSTATE));
-//    	this.addState(new PlayState(HOSTGAMEPLAYSTATE));
-//    	this.addState(new PlayState(JOINGAMEPLAYSTATE));
-//    	this.addState(new EndState(ENDGAMESTATE));
+    	//this.addState(new MapMenuState(1));
+    	this.addState(new MenuState(MAINMENUSTATE));
+    	this.addState(new PlayState(HOSTGAMEPLAYSTATE));
+    	this.addState(new PlayState(JOINGAMEPLAYSTATE));
+    	this.addState(new EndState(ENDGAMESTATE));
     }
 }
